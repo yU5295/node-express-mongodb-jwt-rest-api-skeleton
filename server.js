c62b2425a4f6fe098295487a94b28fc7901b8f69
whale-watching <chenchrissh@gmail.com>
2022-09-24 09:40:58 -0700

    change commet

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const i18n = require('i18n')
 const initMongo = require('./config/mongo')
 const path = require('path')
 
-//Collaborate with Chris and Denial to make a wonderful Restful API server.
+//Collaborate with Chris and Denial to make a wonderful Restful API.
 
 // Setup express server port from ENV, default: 3000
 app.set('port', process.env.PORT || 3000)
